ff2059954320b5bf462f6ab328993fc70f78e098
Raktim Das <raktimdas720@gmail.com>
2025-06-23 02:48:27 +0530

    Update Main.java

--- Financial Forecasting/Main.java
+++ Financial Forecasting/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input from user
         System.out.print("Enter initial investment amount: ");
         double principal = scanner.nextDouble();
 
@@ -14,7 +13,6 @@ public class Main {
         System.out.print("Enter number of years for forecast: ");
         int years = scanner.nextInt();
 
-        // Forecast
         ForecastCalculator calculator = new ForecastCalculator();
         calculator.calculateForecast(principal, rate, years);
 
