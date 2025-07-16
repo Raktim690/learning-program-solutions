e363a15a9752155ba98fe51dc25fbffc28a2f80e
Raktim Das <raktimdas720@gmail.com>
2025-07-16 14:31:13 +0530

    Create CountryController.java

---
+++ REST – Country Web Service/CountryController.java
@@ -0,0 +1,21 @@
+package com.example.country.controller;
+
+import com.example.country.model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        return Arrays.asList(
+            new Country("IN", "India"),
+            new Country("US", "United States"),
+            new Country("JP", "Japan")
+        );
+    }
+}
