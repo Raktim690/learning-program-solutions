e8b389dfa934e25cb1f9d18ca11846844ce89630
Raktim Das <raktimdas720@gmail.com>
2025-07-16 14:38:10 +0530

    Create JwtUtil.java

---
+++ Authentication Service that Returns JWT/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.example.jwt.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.util.Date;
+import java.security.Key;
+
+public class JwtUtil {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 mins
+            .signWith(key)
+            .compact();
+    }
+}
