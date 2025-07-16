35354eea2c342beec31981d7abbe1a614b581a3f
Raktim Das <raktimdas720@gmail.com>
2025-07-16 14:18:25 +0530

    Create SpringWebMavenApplication.java

---
+++ Spring Web Project using Maven/SpringWebMavenApplication.java
@@ -0,0 +1,11 @@
+package com.example.springwebmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMavenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMavenApplication.class, args);
+    }
+}
