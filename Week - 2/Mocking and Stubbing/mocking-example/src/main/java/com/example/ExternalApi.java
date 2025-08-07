07859730ff617aaa47cedfc0ee2be201bfceb76f
Raktim Das <raktimdas720@gmail.com>
2025-08-07 13:32:26 +0530

    Update ExternalApi.java

--- Week - 2/Mocking and Stubbing/mocking-example/src/main/java/com/example/ExternalApi.java
+++ Week - 2/Mocking and Stubbing/mocking-example/src/main/java/com/example/ExternalApi.java
@@ -2,8 +2,6 @@ package com.example;
 
 public class ExternalApi {
     public String getData() {
-        // This method would normally call an external API.
-        // We will mock this behavior in our test.
         return "Real Data";
     }
 }
